4f917d8ce3e69dfee6fd8a735b6add529c0dd44d
kalana <kalanakd471@gmail.com>
2026-04-05 23:51:29 +0530

    Add OTP service beans to OtpAutoConfiguration

--- src/main/java/io/entgra/proprietary/otp/service/config/OtpAutoConfiguration.java
+++ src/main/java/io/entgra/proprietary/otp/service/config/OtpAutoConfiguration.java
@@ -1,12 +1,46 @@
 package io.entgra.proprietary.otp.service.config;
 
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.entgra.proprietary.otp.service.config.Properties.OtpProperties;
+import io.entgra.proprietary.otp.service.service.OtpDeliveryService;
+import io.entgra.proprietary.otp.service.service.OtpService;
+import io.entgra.proprietary.otp.service.service.impl.OtpDeliveryServiceImpl;
+import io.entgra.proprietary.otp.service.service.impl.OtpServiceImpl;
+import io.entgra.proprietary.otp.service.validation.Normalizer;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @AutoConfiguration
 @EnableConfigurationProperties(OtpProperties.class)
 public class OtpAutoConfiguration {
 
+    @Bean
+    @ConditionalOnMissingBean
+    public OtpService otpService(JdbcTemplate jdbcTemplate,
+                                 OtpDeliveryService otpDeliveryService,
+                                 OtpProperties otpProperties,
+                                 ObjectMapper objectMapper) {
+        return new OtpServiceImpl(jdbcTemplate, otpDeliveryService, otpProperties, objectMapper);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Normalizer normalizer(OtpProperties otpProperties) {
+        return new Normalizer(otpProperties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "otp.delivery", name = "enabled", havingValue = "true")
+    public OtpDeliveryService otpDeliveryService(OtpProperties otpProperties,
+                                                 ObjectMapper objectMapper,
+                                                 Normalizer normalizer) {
+        return new OtpDeliveryServiceImpl(otpProperties, objectMapper, normalizer);
+    }
 }
