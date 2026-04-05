81323a64ef793b7f99f20e0bc108d3f89bf021af
kalana <kalanakd471@gmail.com>
2026-04-05 23:52:42 +0530

    Add fields to OtpValidationResult for validation status, metadata, and username

--- src/main/java/io/entgra/proprietary/otp/service/dto/OtpValidationResult.java
+++ src/main/java/io/entgra/proprietary/otp/service/dto/OtpValidationResult.java
@@ -1,4 +1,16 @@
 package io.entgra.proprietary.otp.service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpValidationResult {
+    private boolean valid;
+    private Map<String, Object> metadata;
+    private String username;
 }
