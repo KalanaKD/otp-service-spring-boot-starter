187bd9095a188fd8b8dc64a63fa3c68720350618
kalana <kalanakd471@gmail.com>
2026-04-05 23:52:32 +0530

    Add OtpSMSDeliveryRequest class with distribution type, message template, and parameters

--- src/main/java/io/entgra/proprietary/otp/service/dto/request/OtpSMSDeliveryRequest.java
+++ src/main/java/io/entgra/proprietary/otp/service/dto/request/OtpSMSDeliveryRequest.java
@@ -1,4 +1,36 @@
 package io.entgra.proprietary.otp.service.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpSMSDeliveryRequest {
+    private DistributionType distributionType;
+    private String msgTemplate;
+    private String priority;
+    private String smsGateway;
+    private String expiryAt;
+    private List<Param> params;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Param {
+        private String key;
+        private String value;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DistributionType {
+        private String type;
+        private List<String> values;
+    }
+
 }
