6972dc04ca1757faaa95ba9de8b691f8ab13fa6a
kalana <kalanakd471@gmail.com>
2026-04-05 23:52:00 +0530

    Add sendOtp method to OtpDeliveryService interface for OTP delivery

--- src/main/java/io/entgra/proprietary/otp/service/service/OtpDeliveryService.java
+++ src/main/java/io/entgra/proprietary/otp/service/service/OtpDeliveryService.java
@@ -1,4 +1,11 @@
 package io.entgra.proprietary.otp.service.service;
 
+import io.entgra.proprietary.otp.service.dto.OtpChannelAddressStatus;
+import io.entgra.proprietary.otp.service.dto.OtpDeliveryStatus;
+
+import java.util.Map;
+
 public interface OtpDeliveryService {
+
+    OtpDeliveryStatus sendOtp(String otp, OtpService.OtpType otpType, Map<String, String> channel);
 }
