cdbe630e1969f2e63ccdd1eb30703807c2732e31
kalana <kalanakd471@gmail.com>
2026-04-08 11:50:36 +0530

    Add MSSQL and Oracle query methods for OTP management

--- src/main/java/io/entgra/proprietary/otp/service/provider/impl/MSSQLQueryProvider.java
+++ src/main/java/io/entgra/proprietary/otp/service/provider/impl/MSSQLQueryProvider.java
@@ -7,4 +7,28 @@ public class MSSQLQueryProvider extends AbstractQueryProvider {
         super(tableName);
     }
 
+    @Override
+    public String markOtpAsUsed() {
+        return "UPDATE " + tableName() +
+                " SET IS_USED = 1 WHERE USERNAME = ? AND OTP_TYPE = ? AND OTP = ?";
+    }
+
+    @Override
+    public String findActiveOtp() {
+        return "SELECT OTP, EXPIRY_TIME, IS_USED, METADATA FROM " + tableName() +
+                " WHERE USERNAME = ? AND OTP_TYPE = ? AND OTP = ? AND IS_USED = 0 AND EXPIRY_TIME > ?";
+    }
+
+    @Override
+    public String deleteUnusedOtps() {
+        return "DELETE FROM " + tableName() +
+                " WHERE USERNAME = ? AND OTP_TYPE = ? AND IS_USED = 0";
+    }
+
+    @Override
+    public String cleanUpExpiredOtps() {
+        return "DELETE FROM " + tableName() +
+                " WHERE EXPIRY_TIME < ? OR IS_USED = 1";
+    }
+
 }
